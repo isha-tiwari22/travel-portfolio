cff6d53adb25ff5959fcd19773c9c842dc84fbec
Isha Tiwari <np03cs4a230203@heraldcollege.edu.np>
2026-08-06 22:59:30 +0545

    Add scroll progress bar

---
+++ js/progress.js
@@ -0,0 +1,15 @@
+const progressBar = document.getElementById("progress-bar");
+
+window.addEventListener("scroll", () => {
+
+    const scrollTop = document.documentElement.scrollTop;
+
+    const scrollHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+
+    const progress = (scrollTop / scrollHeight) * 100;
+
+    progressBar.style.width = progress + "%";
+
+});
